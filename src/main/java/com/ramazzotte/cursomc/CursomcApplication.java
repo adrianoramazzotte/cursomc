b7e6e7346d3d91f8e8915e679879f93229d8d6e2
Adriano Ramazzotte <adriano.ramazzotte@gmail.com>
2019-10-01 12:55:28 -0300

    Produtos e associacao Muitoso para Muitos com Categoria

--- src/main/java/com/ramazzotte/cursomc/CursomcApplication.java
+++ src/main/java/com/ramazzotte/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.ramazzotte.cursomc;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ramazzotte.cursomc.domain.Categoria;
+import com.ramazzotte.cursomc.domain.Produto;
 import com.ramazzotte.cursomc.repositories.CategoriaRepository;
+import com.ramazzotte.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtorepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -23,9 +27,25 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Farmacia");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
+		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
