e1ff6183e50604eee7d2c5c7bf3e6c3e7f8ad1dc
Adriano Ramazzotte <adriano.ramazzotte@gmail.com>
2019-10-04 15:53:27 -0300

    Endpont Cliente disponivel

---
+++ src/main/java/com/ramazzotte/cursomc/resource/ClienteResource.java
@@ -0,0 +1,25 @@
+package com.ramazzotte.cursomc.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramazzotte.cursomc.domain.Categoria;
+import com.ramazzotte.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class ClienteResource {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.busca(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/ramazzotte/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.ramazzotte.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramazzotte.cursomc.domain.Cliente;
+import com.ramazzotte.cursomc.repositories.ClienteRepository;
+import com.ramazzotte.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+	
+
+	public Cliente busca(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
