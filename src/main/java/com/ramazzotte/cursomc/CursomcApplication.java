1e961d526b1e77ca395745065a4aa60763bfaf9c
Adriano Ramazzotte <adriano.ramazzotte@gmail.com>
2019-10-06 10:32:39 -0300

    Categoria/page paginação

--- src/main/java/com/ramazzotte/cursomc/CursomcApplication.java
+++ src/main/java/com/ramazzotte/cursomc/CursomcApplication.java
@@ -60,6 +60,14 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama");
+		Categoria cat4 = new Categoria(null, "Mesa e Banho");
+		Categoria cat5 = new Categoria(null, "Prudutos naturais");
+		Categoria cat6 = new Categoria(null, "Jardinagem");
+		Categoria cat7 = new Categoria(null, "outros7");
+		Categoria cat8 = new Categoria(null, "outros8");
+		Categoria cat9 = new Categoria(null, "outros9");
+		Categoria cat10 = new Categoria(null, "outros10");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -72,7 +80,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat9,cat3,cat4,cat5,cat6,cat7,cat8,cat10));
 		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/ramazzotte/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/ramazzotte/cursomc/resource/CategoriaResource.java
@@ -1,17 +1,22 @@
 package com.ramazzotte.cursomc.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ramazzotte.cursomc.domain.Categoria;
+import com.ramazzotte.cursomc.dto.CategoriaDto;
 import com.ramazzotte.cursomc.services.CategoriaService;
 
 @RestController
@@ -43,4 +48,29 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDto>> findAll(){
+		List<Categoria>list = service.findAll();
+		List<CategoriaDto> listDto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);		
+	}
+	
+	@RequestMapping(value="/page",method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDto>> findPage(
+			@RequestParam(value="page",defaultValue = "0")Integer page,
+			@RequestParam(value="linesPerPage",defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value="direction",defaultValue = "nome")String direction,
+			@RequestParam(value="orderBy",defaultValue = "ASC")String orderBy){
+		Page<Categoria>list =service.findPage(page,linesPerPage,direction,orderBy);
+		Page<CategoriaDto>listDto = list.map(obj -> new CategoriaDto(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+		
+			
+			
+			
+	
+	
+		
 }
