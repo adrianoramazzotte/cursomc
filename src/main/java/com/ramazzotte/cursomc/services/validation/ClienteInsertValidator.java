9ff226378cf4e25461be4b957d21370301ae62de
Adriano Ramazzotte <adriano.ramazzotte@gmail.com>
2019-10-11 16:16:41 -0300

    Validação de email - não pode dois email no banco

--- src/main/java/com/ramazzotte/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ramazzotte/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ramazzotte.cursomc.domain.Cliente;
 import com.ramazzotte.cursomc.domain.enuns.TipoCliente;
 import com.ramazzotte.cursomc.dto.ClienteNewDTO;
+import com.ramazzotte.cursomc.repositories.ClienteRepository;
 import com.ramazzotte.cursomc.resource.exception.FieldMessage;
 import com.ramazzotte.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -23,6 +30,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod())&& !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email","Email já exixtente"));
+		}
+		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())&&!BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
 		}
